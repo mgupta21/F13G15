c9fece3a631277d497d49fb6c6212d9b63440a13
mgupta21 <mgupta21@uic.edu>
2013-10-14 20:56:26 -0500

    Adding User Service class

---
+++ F13G15x/src/org/java/app/service/UserService.java
@@ -0,0 +1,19 @@
+package org.java.app.service;
+
+import java.util.List;
+
+import org.java.app.model.UserProfile;
+
+	public class UserService {
+
+	
+	public String createNewUser(UserProfile userProfile) {
+	
+		return "toAdd";
+	}
+	
+	public List<Object> getUserTables(String userName) {
+		
+		return null;
+	}
+}
